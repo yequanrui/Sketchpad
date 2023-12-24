d9f59977fc32668b1f9bd5a66e20b23c676de5eb
yequanrui <yequanrui@qq.com>
2023-12-25 01:46:50 +0800

    update

--- Sketchpad/Properties/AssemblyInfo.cs
+++ Sketchpad/Properties/AssemblyInfo.cs
@@ -9,15 +9,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Designed by FY")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FY")]
-[assembly: AssemblyProduct("Sketchpad made by VS2015(C#)")]
-[assembly: AssemblyCopyright("Copyright © 2011-2017")]
+[assembly: AssemblyProduct("Sketchpad made by VS2022(C#)")]
+[assembly: AssemblyCopyright("Copyright © 2011-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 //将 ComVisible 设置为 false 将使此程序集中的类型
 //对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
 //请将此类型的 ComVisible 特性设置为 true。
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("a5bd460d-6b5b-44ef-936e-c3ce7fc97090")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.8.1.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
