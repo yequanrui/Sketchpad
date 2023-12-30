2d3a7a5e817badbe62d40abe575f146429b5777b
yequanrui <yequanrui@qq.com>
2023-12-30 21:25:39 +0800

    add

---
+++ Sketchpad/Welcome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Sketchpad
+{
+    public partial class Welcome : Form
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
